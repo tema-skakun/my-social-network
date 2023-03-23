fab3e3e080bc792ca8a6b3ac5159e110021c5b67
Farengar Darkhawk <fdarkhaw@c2s16d99.42wolfsburg.de>
2023-03-23 19:42:03 +0100

    lesson 11

--- projects/01-first-project/react-kabzda-1/src/App.js
+++ projects/01-first-project/react-kabzda-1/src/App.js
@@ -2,32 +2,49 @@ import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-    </div>
-  );
-}
-
-const Technologies = () => {
-  return (
-    <div>
-      <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
-    </div>
-  );
-}
-
-const Header = () => {
-  return (
-    <div>
-      <a href= '#s'>Home</a>
-      <a href= '#s'>New Feed</a>
-      <a href= '#s'>Messages</a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPHRvtFUvNT9Rrpz2HE4gu05hPPg8m7DweCg&usqp=CAU' />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/main_image_star-forming_region_carina_nircam_final-1280.jpg'/>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+        </div>
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
