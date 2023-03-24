85c9e42089c00aba73e781b8f380d51366185c23
Юля Грезина <ulagrezina@MacBook-Pro-admin.local>
2023-03-24 10:13:32 +0100

    12 lesson

--- projects/01-first-project/react-kabzda-1/src/App.js
+++ projects/01-first-project/react-kabzda-1/src/App.js
@@ -1,50 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPHRvtFUvNT9Rrpz2HE4gu05hPPg8m7DweCg&usqp=CAU' />
-      </header>
-      <nav className='nav'>
-        <div>
-          <a>Profile</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-      <div className='content'>
-        <div>
-          <img src='https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/main_image_star-forming_region_carina_nircam_final-1280.jpg'/>
-        </div>
-        <div>
-          ava + description
-        </div>
-        <div>
-          My posts
-        </div>
-        <div>
-          New post
-        </div>
-        <div>
-          <div>
-            post 1
-          </div>
-          <div>
-            post 2
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Navbar/>
+      <Profile />
     </div>
   );
 }
---
+++ projects/01-first-project/react-kabzda-1/src/components/Profile.jsx
@@ -0,0 +1,30 @@
+
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img src='https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/main_image_star-forming_region_carina_nircam_final-1280.jpg'/>
+			</div>
+			<div>
+				ava + description
+			</div>
+			<div>
+				My posts
+			</div>
+			<div>
+				New post
+			</div>
+			<div>
+				<div>
+					post 1
+				</div>
+				<div>
+					post 2
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Profile;
