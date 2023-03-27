1fc67886d5cd2b3865287e41bf91f45eef4d40e2
Farengar Darkhawk <fdarkhaw@c1s3d10.42wolfsburg.de>
2023-03-27 19:28:13 +0200

    lesson 22

--- projects/01-first-project/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
+++ projects/01-first-project/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,43 @@
+import style from './Dialogs.module.css'
+import React from "react";
+import {NavLink} from "react-router-dom";
+
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div>
+                    <NavLink
+                        className={style.dialog + ' ' + style.active}
+                        to='/dialogs/1'
+                    >
+                        Linus
+                    </NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/2'>Artem</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/3'>Boris</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/4'>Paul</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/5'>Evgeniy</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/6'>Anton</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/7'>Pablo</NavLink>
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How are you?</div>
+                <div className={style.message}>Yo =)</div>
+            </div>
         </div>
     )
 }
