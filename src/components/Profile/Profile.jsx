20f563c596925bc6e58e038254ce123b167c7cd5
Юля Грезина <ulagrezina@MacBook-Pro-admin.local>
2023-07-06 12:02:02 +0200

    add onClick to profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,26 @@
 import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 import style from './Profile.module.css';
+import React from "react";
 
 
 const Profile = (props) => {
+
+	let newPostElement = React.createRef();
+
+	let addPost = () => {
+		let text = newPostElement.current.value;
+		alert(text);
+	}
+
 	return (
 		<div>
 			<ProfileInfo />
 			<div className={style.newPost}>
-				<textarea></textarea>
+				<textarea ref = {newPostElement}></textarea>
 			</div>
 			<div className={style.newPost}>
-				<button>Add post</button>
+				<button onClick={ addPost }>Add post</button>
 			</div>
 			<div>
 				<MyPosts posts={props.state.posts} />
