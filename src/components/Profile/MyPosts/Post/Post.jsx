9a8d62432fd16b5636f1dd3a6f70c2e00b0f754c
Artem Skakun <artemskakun@MacBook-Pro-admin.local>
2023-07-31 16:01:35 +0200

    add some changes to posts layout

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -3,11 +3,16 @@ import style from './Post.module.css'
 const Post = (props) => {
 	return (
 		<div className={style.item}>
-			<img alt={'avatar'}
-				 src='https://static.vecteezy.com/system/resources/previews/004/819/322/non_2x/female-avatar-woman-profile-icon-for-network-vector.jpg' />
-			{props.message}
-			<div>
-				<span>like</span> {props.likesCount}
+			<div className={style.avatar}>
+				<img alt={'avatar'}
+					  src='https://static.vecteezy.com/system/resources/previews/004/819/322/non_2x/female-avatar-woman-profile-icon-for-network-vector.jpg' />
+			</div>
+			<div className={style.msg}>
+				{props.message}
+			</div>
+			<div className={style.like}>
+				<span>like(s) </span>
+				{props.likesCount}
 			</div>
 		</div>
 	)
