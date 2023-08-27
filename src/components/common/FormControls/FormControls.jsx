0a5bf3f53169c5de31f3a1f14e0d76ae52fe98c9
tema-skakun <frederico_f@mail.ru>
2023-08-27 19:59:35 +0200

    refactored into FormControls

--- src/components/common/FormControls/FormControls.jsx
+++ src/components/common/FormControls/FormControls.jsx
@@ -1,25 +1,45 @@
 import style from "./FormControls.module.css"
 
-export const Textarea = ({input, meta, ...props}) => {
+const FormControl = ({input, meta, ...props}) => {
     const hasError = meta.touched && meta.error;
     return (
         <div className={style.formControl + " " + (hasError ? style.error : "")}>
             <div>
-                <textarea {...input} {...props}/>
+                <props.typeField {...input} {...props}/>
             </div>
             {hasError && <span>{meta.error}</span>}
         </div>
     )
 }
 
-export const Input = ({input, meta, ...props}) => {
-    const hasError = meta.touched && meta.error;
-    return (
-        <div className={style.formControl + " " + (hasError ? style.error : "")}>
-            <div>
-                <input {...input} {...props}/>
-            </div>
-            {hasError && <span>{meta.error}</span>}
-        </div>
-    )
+export const Textarea = (props) => {
+    return <FormControl typeField="textarea" {...props}/>
 }
+export const Input = (props) => {
+    return <FormControl typeField="input" {...props}/>
+}
+
+//
+// export const Textarea = ({input, meta, ...props}) => {
+//     const hasError = meta.touched && meta.error;
+//     return (
+//         <div className={style.formControl + " " + (hasError ? style.error : "")}>
+//             <div>
+//                 <textarea {...input} {...props}/>
+//             </div>
+//             {hasError && <span>{meta.error}</span>}
+//         </div>
+//     )
+// }
+
+// export const Input = ({input, meta, ...props}) => {
+//     const hasError = meta.touched && meta.error;
+//     return (
+//         <div className={style.formControl + " " + (hasError ? style.error : "")}>
+//             <div>
+//                 <input {...input} {...props}/>
+//             </div>
+//             {hasError && <span>{meta.error}</span>}
+//         </div>
+//     )
+// }
