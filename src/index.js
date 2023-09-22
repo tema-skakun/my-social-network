91ab8c34ef2aa55762a016e7f644d9a1ce53b27a
tema-skakun <frederico_f@mail.ru>
2023-09-22 19:35:31 +0200

    added HashRouter for correct work on githab pages

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './redux/redux-toolkit-store';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 export const rerenderEntireTree = () => {
     root.render(
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <React.StrictMode>
                 <Provider store={store}>
                     <App/>
                 </Provider>
             </React.StrictMode>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
