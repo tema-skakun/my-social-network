0db63866051630f4efd0d3afb6f9575d63a884e4
tema-skakun <frederico_f@mail.ru>
2023-10-05 19:05:25 +0200

    WIP: added layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App_antd';
 import store from './redux/redux-toolkit-store';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
